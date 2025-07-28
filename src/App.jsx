f7a38b2fb36031c49b7dc1e7d73d2536cf520107
Atharva251 <patharva266@gmail.com>
2025-07-28 15:10:37 +0530

    Changes added: - Installed React Router DOM for routing. - Created basic Routes. - Added components for Body, Footer, Login, NavBar, and Profile. - Updated App component to include routing and layout structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import './index.css'
+import './App.css'
+import { Routes, Route } from 'react-router'
+import { BrowserRouter as BrouserRouter } from 'react-router-dom'
+import Body from './Body.jsx'
+import Login from './Login.jsx'
+import Profile from './Profile.jsx'
 
 function App() {
 
   return (
     <>
-      <div>
-        <p>Hello World</p>
-      </div>
+      <BrouserRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Body />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrouserRouter >
+
     </>
   )
 }
